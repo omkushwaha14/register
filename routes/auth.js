8113cb3be6c28e2421da7bd4f1cde9b820ef77ab
Om Nath Kushwaha <58984004+omkushwaha14@users.noreply.github.com>
2020-06-10 13:39:04 +0545

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { signup, signin, signout, forgotPassword, resetPassword  } = require('../controllers/auth');
+const { signup, signin, signout, forgotPassword, resetPassword,socialLogin  } = require('../controllers/auth');
 
 // import password reset validator
 const { userSignupValidator, userSigninValidator, passwordResetValidator } = require('../validator');
@@ -14,7 +14,7 @@ router.get('/signout', signout);
 // password forgot and reset routes
 router.put('/forgot-password', forgotPassword);
 router.put('/reset-password', passwordResetValidator, resetPassword);
-
+router.post('/social-login', socialLogin);
 
 
 
