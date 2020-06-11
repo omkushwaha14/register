2aae108a57742afd6ecc9e21a399a5db0cc655aa
Om Nath Kushwaha <58984004+omkushwaha14@users.noreply.github.com>
2020-06-11 16:19:28 +0545

    Update Menu.js

--- client/src/core/Menu.js
+++ client/src/core/Menu.js
@@ -3,14 +3,14 @@ import { Link, withRouter } from 'react-router-dom';
 import { signout, isAuthenticated } from '../auth';
 
 const isActive = (history, path) => {
-    if (history.location.pathname === path) return { color: '#ff9900' };
+    if (history.location.pathname === path) return { color: '#ff2900' };
     else return { color: '#ffffff' };
 };
 
 const Menu = ({ history }) => (
     <div>
         <ul className="nav nav-tabs bg-primary">
-            <li className="nav-item">
+            <li className="navbar navbar-dark bg-primary">
                 <Link className="nav-link" style={isActive(history, '/')} to="/">
                     Home
                 </Link>
@@ -18,46 +18,51 @@ const Menu = ({ history }) => (
 
 
 
-
+            <li className="navbar navbar-dark bg-primary">
+                <Link to={`/post/create`} style={isActive(history, `/post/create`)} className="nav-link">
+                     Post
+                </Link>
+            </li>
 
             {!isAuthenticated() && (
                 <React.Fragment>
-                    <li className="nav-item">
+                    <li className="navbar navbar-dark bg-primary">
                         <Link className="nav-link" style={isActive(history, '/signin')} to="/signin">
-                            Sign In
+                            Login
                         </Link>
                     </li>
-                    <li className="nav-item">
+                    <li className="navbar navbar-dark bg-primary">
                         <Link className="nav-link" style={isActive(history, '/signup')} to="/signup">
-                            Sign Up
+                            Register
                         </Link>
                     </li>
                 </React.Fragment>
             )}
 
 
-
             {isAuthenticated() && (
                 <React.Fragment>
-
-
-                    <li className="nav-item">
-                        <Link
-                            to={`/user/${isAuthenticated().user._id}`}
-                            style={isActive(history, `/user/${isAuthenticated().user._id}`)}
-                            className="nav-link"
-                        >
-                            {`${isAuthenticated().user.name}'s profile`}
+                    <li className="navbar navbar-dark bg-primary">
+                        <Link to={`/findpeople`} style={isActive(history, `/findpeople`)} className="nav-link">
+                            Friends
                         </Link>
                     </li>
 
-                    <li className="nav-item">
+                    <li className="navbar navbar-dark bg-primary">
+                        <Link
+                            to={`/user/${isAuthenticated().user._id}`}
+                            style={isActive(history, `/user/${isAuthenticated().user._id}`)}
+                            className="nav-link">Profile
+
+                        </Link>
+                    </li>
+
+                    <li className="navbar navbar-dark bg-primary">
                         <span
                             className="nav-link"
                             style={{ cursor: 'pointer', color: '#fff' }}
-                            onClick={() => signout(() => history.push('/'))}
-                        >
-                            Sign Out
+                            onClick={() => signout(() => history.push('/'))}>
+                            Logout
                         </span>
                     </li>
                 </React.Fragment>
