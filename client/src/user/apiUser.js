45fe3e100d5b1aa6d956c385e89d109c8e8465b0
Om Nath Kushwaha <58984004+omkushwaha14@users.noreply.github.com>
2020-06-11 17:34:18 +0545

    Add files via upload

---
+++ client/src/user/apiUser.js
@@ -0,0 +1,113 @@
+export const read = (userId, token) => {
+    return fetch(`/api/user/${userId}`, {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        }
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+export const update = (userId, token, user) => {
+    console.log("USER DATA UPDATE: ", user);
+    return fetch(`/api/user/${userId}`, {
+        method: "PUT",
+        headers: {
+            Accept: "application/json",
+            Authorization: `Bearer ${token}`
+        },
+        body: user
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+export const remove = (userId, token) => {
+    return fetch(`/api/user/${userId}`, {
+        method: "DELETE",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        }
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+export const list = () => {
+    return fetch(`/api/users`, {
+        method: "GET"
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+export const updateUser = (user, next) => {
+    if (typeof window !== "undefined") {
+        if (localStorage.getItem("jwt")) {
+            let auth = JSON.parse(localStorage.getItem("jwt"));
+            auth.user = user;
+            localStorage.setItem("jwt", JSON.stringify(auth));
+            next();
+        }
+    }
+};
+
+export const follow = (userId, token, followId) => {
+    return fetch(`/api/user/follow`, {
+        method: "PUT",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify({ userId, followId })
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+export const unfollow = (userId, token, unfollowId) => {
+    return fetch(`/api/user/unfollow`, {
+        method: "PUT",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify({ userId, unfollowId })
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+export const findPeople = (userId, token) => {
+    return fetch(`/api/user/findpeople/${userId}`, {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        }
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
