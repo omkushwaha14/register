d0f6730d180b24908dcbe91b1639a750bb85b355
Om Nath Kushwaha <58984004+omkushwaha14@users.noreply.github.com>
2020-06-12 14:34:55 +0545

    Update PrivateRoute.js

--- client/src/auth/PrivateRoute.js
+++ client/src/auth/PrivateRoute.js
@@ -3,21 +3,22 @@ import { Route, Redirect } from "react-router-dom";
 import { isAuthenticated } from "./index";
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
-  
-  
 
-   <Route
+    <Route
         {...rest}
-           render={props=>
-               !isAuthenticated ?
-                   (<Redirect to='/signin'/>
-                   ):(
-            <Component {...props}/>
+        render={props =>
+            isAuthenticated() ? (
+                <Component {...props} />
+            ) : (
+                <Redirect
+                    to={{
+                        pathname: "/signin",
+                        state: { from: props.location }
+                    }}
+                />
             )
-           }
+        }
     />
-
-
 );
 
 export default PrivateRoute;
