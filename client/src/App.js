7c3159eeedef9d98ae0c3178d6d577d21925145e
Om Nath Kushwaha <58984004+omkushwaha14@users.noreply.github.com>
2020-06-12 13:49:18 +0545

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,54 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import MainRouter from "./MainRouter";
+import React ,{Fragment} from 'react';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import Home from "./core/Home";
+import Menu from "./core/Menu";
+import Signup from "./user/Signup";
+import Signin from "./user/Signin";
+import Profile from "./user/Profile";
+import Users from "./user/Users";
+import EditProfile from "./user/EditProfile";
+import FindPeople from "./user/FindPeople";
+import NewPost from "./post/NewPost";
+import EditPost from "./post/EditPost";
+import SinglePost from "./post/SinglePost";
+import PrivateRoute from "./auth/PrivateRoute";
+import ForgotPassword from "./user/ForgotPassword";
+import ResetPassword from "./user/ResetPassword";
 
-const App = () => (
-    <BrowserRouter>
-    
-      <MainRouter />
-   
-    </BrowserRouter>
-);
+const App = () => {
+    return(
+        <Router>
+            <Fragment>
+                <Menu />
+                <section className="container">
+                <Switch>
+
+                    <Route exact path="/" component={Home} />
+
+                    <Route exact path="/forgot-password" component={ForgotPassword} />
+                    <Route exact path="/reset-password/:resetPasswordToken" component={ResetPassword}
+                    />
+                    <PrivateRoute exact path="/post/create" component={NewPost} />
+                    <Route exact path="/post/:postId" component={SinglePost} />
+                    <PrivateRoute
+                        exact
+                        path="/post/edit/:postId"
+                        component={EditPost}
+                    />
+                    <Route exact path="/users" component={Users} />
+                    <Route exact path="/signup" component={Signup} />
+                    <Route exact path="/signin" component={Signin} />
+                    <PrivateRoute exact path="/user/edit/:userId" component={EditProfile}/>
+                    <PrivateRoute exact path="/findpeople" component={FindPeople} />
+                    <PrivateRoute exact path="/user/:userId" component={Profile} />
+                    </Switch>
+                </section>
+                    </Fragment>
+        </Router>
+
+                    )
+}
+
+
 
 export default App;
