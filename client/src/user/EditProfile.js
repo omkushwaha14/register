13b41ed414e24ecca6a25ed82fe3526ba23f963a
Om Nath Kushwaha <58984004+omkushwaha14@users.noreply.github.com>
2020-06-13 23:39:45 +0545

    Update EditProfile.js

--- client/src/user/EditProfile.js
+++ client/src/user/EditProfile.js
@@ -45,9 +45,9 @@ class EditProfile extends Component {
 
   isValid = () => {
     const { name, email, password, fileSize } = this.state;
-    if (fileSize > 1000000) {
+    if (fileSize > 3000000) {
       this.setState({
-        error: "File size should be less than 100kb",
+        error: "File size should be less than 300kb",
         loading: false
       });
       return false;
