312b87aaf7a7b508c91e7b78fe0de2ef788eaf1d
Om Nath Kushwaha <58984004+omkushwaha14@users.noreply.github.com>
2020-06-13 23:27:21 +0545

    Update NewPost.js

--- client/src/post/NewPost.js
+++ client/src/post/NewPost.js
@@ -25,9 +25,9 @@ class NewPost extends Component {
 
     isValid = () => {
         const { title, body, fileSize } = this.state;
-        if (fileSize > 100000) {
+        if (fileSize > 500000) {
             this.setState({
-                error: "File size should be less than 100kb",
+                error: "File size should be less than 500kb",
                 loading: false
             });
             return false;
