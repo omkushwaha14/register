3759e6fbfa63b4efea86e2e397038ef62515b527
Om Nath Kushwaha <58984004+omkushwaha14@users.noreply.github.com>
2020-06-13 21:51:03 +0545

    Update Signin.js

--- client/src/user/Signin.js
+++ client/src/user/Signin.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { Link, Redirect } from "react-router-dom";
 import { signin, authenticate } from "../auth";
-import SocialLogin from "./SocialLogin";
 
+import Spinner from "../core/Spinner";
 class Signin extends Component {
     constructor() {
         super();
@@ -95,9 +95,9 @@ class Signin extends Component {
             <div className="container">
                 <h2 className="mt-5 mb-5">SignIn</h2>
                 <hr />
-                <SocialLogin/>
 
-                <hr />
+
+
                 <br />
 
                 <div
@@ -107,11 +107,8 @@ class Signin extends Component {
                     {error}
                 </div>
 
-                {loading ? (
-                    <div className="jumbotron text-center">
-                        <h2>Loading...</h2>
-                    </div>
-                ) : (
+                {loading ? (<Spinner/>
+                    ) : (
                     ""
                 )}
 
