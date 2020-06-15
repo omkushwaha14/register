7de2600e84fa9c293577fed314d2dbda26ec1f06
Om Nath Kushwaha <58984004+omkushwaha14@users.noreply.github.com>
2020-06-15 15:46:57 +0545

    Update Home.js

--- client/src/core/Home.js
+++ client/src/core/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import Posts from "../post/Posts";
 
 const Home = () => (
-    <div>
+    <div className="container">
         <div className="jumbotron">
             <h2>welcome to wordbok</h2>
             <p className="lead">here you can create blogs,share memes, comedy pictures </p>
